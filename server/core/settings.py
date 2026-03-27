687dac8b099321cfcf7f34258a2f6348510bef8a
Ndukwe Daniel <pomiledaniel@gmail.com>
2026-03-27 01:18:19 +0100

    Cloudinary config

--- server/core/settings.py
+++ server/core/settings.py
@@ -132,7 +132,14 @@ MEDIA_ROOT = BASE_DIR / 'media'
 if not DEBUG:
     import cloudinary
     cloudinary.config(secure=True)
-    DEFAULT_FILE_STORAGE = 'cloudinary_storage.storage.MediaCloudinaryStorage'
+    STORAGES = {
+        "default": {
+            "BACKEND": "cloudinary_storage.storage.MediaCloudinaryStorage",
+        },
+        "staticfiles": {
+            "BACKEND": "django.contrib.staticfiles.storage.StaticFilesStorage",
+        },
+    }
 
 # REST framework
 REST_FRAMEWORK = {
